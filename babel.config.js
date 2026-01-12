96d04504c001ac5b41c863ba293154e212957841
derlisrd <derlisruizdiaz@hotmail.com>
2026-01-12 10:22:46 -0300

    fixing env

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  // Elimina "react-native-dotenv" de la lista de presets
+  presets: ['module:@react-native/babel-preset'], 
+  plugins: [
+    [
+      'module:react-native-dotenv', // Este nombre es el correcto
+      {
+        "moduleName": "@env",
+        "path": ".env",
+        "blacklist": null,
+        "whitelist": null,
+        "safe": false,
+        "allowUndefined": true
+      }
+    ]
+  ]
 };
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,16 +1,19 @@
-import { NewsResponse } from "./dto/news-response";
-const BASE_URL = 'https://newsapi.org/v2';
-const API_KEY = '37e5ba8017aa4c0999e8b04403fe0c19';
+
+import { NewsResponse } from './dto/news-response';
+
+const BASE_URL = `${process.env.API_URL}`;
+const API_KEY =  `${process.env.API_KEY}` 
 
 export const API = {
   todos: async (busqueda: string = '') => {
     try {
-
+      console.log({
+        BASE_URL, API_KEY
+      })
       const url =
         busqueda === ''
           ? `${BASE_URL}/top-headlines?country=us&apiKey=37e5ba8017aa4c0999e8b04403fe0c19`
           : `${BASE_URL}/everything?apiKey=${API_KEY}&language=es&q=${busqueda}`;
-
 
       const response = await fetch(url);
       const data = await response.json();
