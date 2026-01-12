d95c707626bba1b2c19396dfddc9a4545eee16a2
derlisrd <derlisruizdiaz@hotmail.com>
2026-01-12 09:12:24 -0300

    fixing

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import { NewsResponse } from "./dto/news-response";
-
+const BASE_URL = 'https://newsapi.org/v2';
 const API_KEY = '37e5ba8017aa4c0999e8b04403fe0c19';
 
 export const API = {
@@ -8,8 +8,8 @@ export const API = {
 
       const url =
         busqueda === ''
-          ? `https://newsapi.org/v2/top-headlines?country=us&apiKey=37e5ba8017aa4c0999e8b04403fe0c19`
-          : `https://newsapi.org/v2/everything?apiKey=${API_KEY}&language=es&q=${busqueda}`;
+          ? `${BASE_URL}/top-headlines?country=us&apiKey=37e5ba8017aa4c0999e8b04403fe0c19`
+          : `${BASE_URL}/everything?apiKey=${API_KEY}&language=es&q=${busqueda}`;
 
 
       const response = await fetch(url);
